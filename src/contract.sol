659853a839a87c264030fb8eab1d4671e3b072c5
mktcode <kontakt@markus-kottlaender.de>
2021-08-02 17:51:29 +0200

    renamed issueSums to issueBalances and added getIssueBalance function

--- src/contract.sol
+++ src/contract.sol
@@ -14,7 +14,7 @@ contract Deposits {
     uint256 nextDepositId = 0;
     mapping(address => uint256[]) depositsIdsBySender;
     mapping(uint256 => Deposit) depositsById;
-    mapping(string => uint256) issueSums;
+    mapping(string => uint256) issueBalances;
     mapping(string => bool) withdrawnIssues;
     
     constructor() {
@@ -34,9 +34,13 @@ contract Deposits {
         return (depositsById[_depositId].sender, depositsById[_depositId].issueId, depositsById[_depositId].value);
     }
 
+    function getIssueBalance(string calldata _issueId) public view returns(uint256) {
+        return issueBalances[_issueId];
+    }
+
     function deposit(string calldata _issueId) public payable {
         require(withdrawnIssues[_issueId] == false, "Issue has already been withdrawn.");
-        issueSums[_issueId] += msg.value;
+        issueBalances[_issueId] += msg.value;
         depositsById[nextDepositId] = Deposit(msg.sender, _issueId, msg.value);
         depositsIdsBySender[msg.sender].push(nextDepositId);
         nextDepositId++;
@@ -48,18 +52,18 @@ contract Deposits {
         require(withdrawnIssues[depositsById[_depositId].issueId] == false, "Deposits for this issue have already been withdrawn.");
         
         uint256 value = depositsById[_depositId].value;
-        issueSums[depositsById[_depositId].issueId] -= depositsById[_depositId].value;
+        issueBalances[depositsById[_depositId].issueId] -= depositsById[_depositId].value;
         delete depositsById[_depositId];
         payable(msg.sender).transfer(value);
     }
 
     function withdraw(string calldata _issueId, address _to) public {
         require(msg.sender == owner, "Caller is not owner");
-        require(issueSums[_issueId] > 0, "Issue has no deposits.");
+        require(issueBalances[_issueId] > 0, "Issue has no deposits.");
         require(withdrawnIssues[_issueId] == false, "Issue has already been withdrawn.");
         
-        uint256 value = issueSums[_issueId];
-        issueSums[_issueId] = 0;
+        uint256 value = issueBalances[_issueId];
+        issueBalances[_issueId] = 0;
         withdrawnIssues[_issueId] = true;
         payable(_to).transfer(value);
     }
