4183f9e5ea63ad0b82e9c4003063e65b6b5f9a84
mktcode <kontakt@markus-kottlaender.de>
2021-08-04 03:30:14 +0200

    adjusted grid gap, added issue placeholders

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container mx-auto py-10 px-8 grid gap-4 md:gap-8 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+  <div class="container mx-auto py-10 px-4 md:px-8 grid gap-4 md:gap-8 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+    <Issue />
+    <Issue />
+    <Issue />
     <Issue />
     <Issue />
     <Issue />
