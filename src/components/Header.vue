e5d7b1bbe167361382dada6e2157c9f5690e8366
mktcode <kontakt@markus-kottlaender.de>
2021-08-04 04:51:52 +0200

    changed title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,12 @@
   <div class="px-8 py-5 bg-gradient-to-br from-indigo-1100 via-gray-900 to-gray-800 text-white text-xl">
     <div class="container mx-auto ">
       <div class="flex justify-between items-center">
-        <h1 class="font-extrabold text-4xl font-brand">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 inline opacity-50" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <h1 class="font-extrabold text-4xl font-brand text-gray-200">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
           </svg>
-          booster
+          <span class="text-indigo-700">eth</span>booster
         </h1>
         <div class="space-x-5">
           <a href="#" class="text-gray-400 hover:text-gray-100">
