a666f7e86ec57ab93ff06300a43e6fcb8d4948d8
mktcode <kontakt@markus-kottlaender.de>
2021-08-04 02:23:39 +0200

    added twitter link and fixed pixel issue

--- src/components/Issue.vue
+++ src/components/Issue.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative bg-gradient-to-r from-white to-gray-800 rounded-3xl">
+  <div class="relative bg-gradient-to-r from-gray-100 to-gray-800" style="border-radius: 1.7rem">
     <a href="https://github.com/mktcode/mktcode/issues/3" target="__blank" class="p-5 rounded-3xl shadow-md bg-gray-800 hover:bg-gray-900 text-white block relative z-10">
       <div class="text-gray-500">trezor/trezor-firmware</div>
       <h3 class="font-bold text-xl truncate">Adding support of EIP-712 into Ethereum app</h3>
@@ -9,24 +9,29 @@
         <div class="bg-gray-700 rounded-full px-2 py-1 text-xs text-gray-300">help-wanted</div>
       </div>
     </a>
-    <div class="rounded-b-3xl overflow-hidden shadow-md text-gray-800 relative z-0 -mt-5 pt-5 flex">
-      <div class="bg-white flex items-center">
-        <div class="text-2xl px-5 font-bold whitespace-nowrap">
+    <div class="rounded-b-3xl shadow-md text-gray-800 relative z-0 -mt-5 pt-5 flex">
+      <div class="bg-gray-100 rounded-bl-3xl flex items-center">
+        <div class="text-2xl px-3 font-bold whitespace-nowrap">
           0.75 ETH
         </div>
-        <input type="number" class="w-full border-0 text-2xl text-right focus:ring-0 px-5" placeholder="+0.00" />
+        <input type="number" class="w-full h-full border-0 rounded-full text-2xl text-right focus:ring-0" placeholder="+0.00" />
+      </div>
+      <div class="p-1 bg-gray-100 rounded-r-3xl flex">
         <a href="#" class="text-indigo-900 rounded-full p-1 inline-block hover:text-white hover:bg-indigo-900 hover:shadow-md">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 inline opacity-90" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
           </svg>
         </a>
-      </div>
-      <div class="p-1 bg-white rounded-r-3xl">
         <a href="#" class="text-indigo-900 rounded-full p-1 inline-block hover:text-white hover:bg-indigo-900 hover:shadow-md">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 inline opacity-90" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
           </svg>
+        </a>
+        <a href="#" class="text-indigo-900 rounded-full p-1 inline-block hover:text-white hover:bg-indigo-900 hover:shadow-md">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 inline opacity-90" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m 7.0027808,13.28056 c 0.3781587,0.09097 -1.4858784,0.05887 -1.4858784,0.05887 0,0 0.7349123,2.222575 3.1929439,2.340303 0,0 -1.5233375,1.617877 -5.0730349,1.471608 0,0 3.0127834,2.163711 7.4276076,1.295016 0,0 3.988505,-0.677832 6.136161,-4.532554 0,0 1.384204,-2.088792 1.384204,-5.0320079 0,0 -0.02854,-0.353186 0.338916,-0.6760479 0,0 1.052423,-0.9560994 1.385987,-1.457338 0,0 -1.475176,0.4709145 -1.887226,0.4851847 0,0 1.295015,-1.0595579 1.382419,-1.8693883 0,0 -1.000693,0.5511841 -1.971062,0.7866414 0,0 -0.162323,0.067783 -0.367456,-0.153404 0,0 -0.9561,-1.0880981 -2.811219,-0.8241006 0,0 -2.413437,0.3817262 -2.825487,2.884352 0,0 -0.148053,0.7955604 0.04459,1.266475 0,0 -4.0759091,-0.01427 -7.0779899,-3.590724 0,0 -1.4573381,2.7077591 1.0006936,4.5325536 0,0 -0.4566445,0.146268 -1.4858784,-0.3674564 -0.00178,-0.00357 0.012486,2.7345164 2.6917053,3.3820234 z" />
+        </svg>
         </a>
       </div>
     </div>
