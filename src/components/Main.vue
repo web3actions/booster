43464665241da5a963e142dd4c14bb1b2dfdea24
mktcode <kontakt@markus-kottlaender.de>
2021-08-04 02:33:32 +0200

    adjusted colum breakpoints

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto py-10 px-8 grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+  <div class="container mx-auto py-10 px-8 grid gap-4 md:gap-8 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
     <Issue />
     <Issue />
     <Issue />
