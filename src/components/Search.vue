5d8728dd076e39539b1bbc0a0c5b99be54389557
mktcode <kontakt@markus-kottlaender.de>
2021-08-07 17:52:04 +0200

    made search fetch issue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 top-6 left-5 opacity-20 absolute" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       </svg>
-      <input type="text" placeholder="Enter issue link, label or language" class="w-full p-5 pl-20 text-4xl border-0 focus:ring-0 font-light placeholder-gray-500 focus:placeholder-gray-300" />
+      <input type="text" v-model="search" placeholder="Enter issue link, label or language" class="w-full p-5 pl-20 text-4xl border-0 focus:ring-0 font-light placeholder-gray-500 focus:placeholder-gray-300" />
       <button v-if="sort == 'desc'" @click="toggleSort" class="top-6 right-5 opacity-40 absolute cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
@@ -20,8 +20,20 @@
 </template>
 
 <script setup>
+import { ref, watchEffect } from 'vue'
 import { mapGetters, mapMutations } from '../lib'
 
 const { sort } = mapGetters()
-const { toggleSort } = mapMutations()
+const { toggleSort, setIssue } = mapMutations()
+
+const search = ref('')
+watchEffect(async () => {
+  const issueUrl = search.value.match(/^https:\/\/github\.com\/([\w-]+)\/([\w-]+)\/issues\/(\d+)$/)
+  if (issueUrl) {
+    const response = await fetch(`https://api.github.com/repos/${issueUrl[1]}/${issueUrl[2]}/issues/${issueUrl[3]}`)
+    setIssue(await response.json())
+  } else {
+    setIssue(null)
+  }
+})
 </script>
