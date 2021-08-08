7476bf9334f6ff4d57632d19010b08272276a3af
mktcode <kontakt@markus-kottlaender.de>
2021-08-08 23:22:29 +0200

    moved social icons to new footer component

--- app/App.vue
+++ app/App.vue
@@ -2,10 +2,12 @@
   <Header />
   <Search />
   <Main />
+  <Footer />
 </template>
 
 <script setup>
 import Header from './components/Header.vue'
 import Search from './components/Search.vue'
 import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 </script>
