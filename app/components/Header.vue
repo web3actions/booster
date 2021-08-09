ac7b0bfbeb27c0c945a80a8ff1c062516c9b99b4
mktcode <kontakt@markus-kottlaender.de>
2021-08-09 13:58:10 +0200

    fixed icon code in create bounty modal

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -60,8 +60,7 @@
       It doesn't matter if you are the first or other deposits already exist. The user who solves the issue can withdraw everything at once.
       <b>You can cancel a deposit at any time.</b> Click on
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path v-if="onlyOwn" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-        <path v-else stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
       </svg>
       in the search bar to see only your own bounties. Make sure you are connected to same Ethereum address than the one you made the deposit with.
     </p>
