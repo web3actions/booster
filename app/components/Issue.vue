342c0df2f311ec0aeecd93fb0867fd778728223e
mktcode <kontakt@markus-kottlaender.de>
2021-08-09 20:01:33 +0200

    fixed amount input

--- app/components/Issue.vue
+++ app/components/Issue.vue
@@ -16,10 +16,10 @@
           {{ ethers.utils.formatEther(bounty) }} ETH
         </div>
         <div class="flex items-center h-full bg-white rounded-full filter drop-shadow-2xl">
-          <input v-model.number="amount" type="number" class="w-full max-w-xs h-full pr-0 border-0 rounded-full text-2xl text-right focus:ring-0" placeholder="+0.00" />
+          <input v-model="amount" type="number" class="w-full max-w-xs h-full pr-0 border-0 rounded-full text-2xl text-right focus:ring-0" placeholder="+0.00" />
           <i v-if="waitingForConfirmation" class="fas fa-spinner fa-spin text-gray-400 text-2xl p-2 m-1 mx-2 inline-block" />
           <i v-else-if="showSuccess" class="fas fa-check text-green-700 text-2xl p-2 m-1 mx-2 inline-block" />
-          <button v-else-if="amount" @click="deposit" class="text-indigo-900 rounded-full p-1 m-1 inline-block hover:text-white hover:bg-indigo-900 hover:shadow-md">
+          <button v-else-if="Number(amount)" @click="deposit" class="text-indigo-900 rounded-full p-1 m-1 inline-block hover:text-white hover:bg-indigo-900 hover:shadow-md">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 inline opacity-90" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
