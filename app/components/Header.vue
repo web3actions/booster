156d57fdf9f680be6777747e220c34d86448b55b
mktcode <kontakt@markus-kottlaender.de>
2021-08-09 13:41:21 +0200

    style improvements and create bounty modal

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -32,12 +32,15 @@
             </button>
             <a href="https://medium.com" target="__blank" class="bg-gray-300 hover:bg-white text-gray-800 rounded-xl px-5 py-3 font-extrabold shadow-lg">
               Read more
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
             </a>
           </div>
         </div>
         <div class="w-full max-w-lg">
           <div class="aspect-w-16 aspect-h-9 rounded-3xl overflow-hidden">
-            <iframe src="https://www.youtube-nocookie.com/embed/R8_veQiYBjI" frameborder="0" allowfullscreen></iframe>
+            <iframe src="https://www.youtube-nocookie.com/embed/8G-1NW5NqO0" frameborder="0" allowfullscreen></iframe>
           </div>
         </div>
       </div>
@@ -46,8 +49,27 @@
   <Modal :show="showCreateBountyModal" @close="showCreateBountyModal = false">
     <h1 class="text-3xl font-extrabold mb-5">Creating a bounty is easy!</h1>
     <p>
-      Simply paste the link to an issue into the search field.
+      Simply paste the link to an issue into the search field. When the issue pops up, enter the amount you want to deposit and click on
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+      </svg>.
     </p>
+    <img src="/create-bounty.gif" class="my-5 rounded-2xl" />
+    <p>
+      It doesn't matter if you are the first or other deposits already exist. The user who solves the issue can withdraw everything at once.
+      <b>You can cancel a deposit at any time.</b> Click on
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path v-if="onlyOwn" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+        <path v-else stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+      </svg>
+      in the search bar to see only your own bounties. Make sure you are connected to same Ethereum address than the one you made the deposit with.
+    </p>
+    <div class="text-center mt-5">
+      <button @click="showCreateBountyModal = false" class="text-lg bg-gray-200 hover:bg-gray-50 text-gray-800 rounded-xl px-5 py-2 font-extrabold shadow-lg">
+        Got it.
+      </button>
+    </div>
   </Modal>
 </template>
 
--- app/components/Modal.vue
+++ app/components/Modal.vue
@@ -20,7 +20,7 @@
           leave-from="scale-100 opacity-100"
           leave-to="scale-90 opacity-0"
         >
-          <div class="max-w-xl bg-gradient-to-br from-indigo-1100 to-gray-900 text-white shadow-2xl rounded-3xl p-5 relative">
+          <div class="max-w-xl bg-indigo-800 text-white shadow-2xl rounded-3xl p-5 relative">
             <button @click="$emit('close')" class="absolute top-4 right-4">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
