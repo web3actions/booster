a6557e7db577a496771f43a3e7455ed5e5ec6179
mktcode <kontakt@markus-kottlaender.de>
2021-08-20 17:39:54 +0200

    delay store init. and better loading screen

--- ui/App.vue
+++ ui/App.vue
@@ -7,35 +7,23 @@
       </svg>
       <span class="text-indigo-700">eth</span>booster
     </h1>
-    <Suspense v-if="ethereumEnabled">
+    <Suspense>
       <template #default>
         <Main />
       </template>
       <template #fallback>
-        <svg class="animate-spin text-gray-300 h-12 w-12 my-2 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
+        <div class="text-center">
+          <svg class="animate-spin text-gray-300 h-12 w-12 my-2 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          <div class="text-sm text-gray-300">connecting...</div>
+        </div>
       </template>
     </Suspense>
-    <div v-else class="text-center">
-      <div class="text-lg">
-        Your browser does not support Ethereum.
-      </div>
-      <a
-        href="https://metamask.io"
-        target="__blank"
-        class="inline-block bg-indigo-700 hover:bg-indigo-900 text-white text-xl font-extrabold rounded-xl px-5 py-3 mt-3 shadow-md"
-      >
-        Install MetaMask
-      </a>
-    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import Main from './components/Main.vue'
-
-const ethereumEnabled = ref(!!window.ethereum)
 </script>
--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import store from './store'
 import App from './App.vue'
+import BrowserNote from './BrowserNote.vue'
 import './index.css'
 
-const app = createApp(App)
-app.use(store)
-app.mount('#app')
+if (window.ethereum) {
+  const app = createApp(App)
+  app.use(store.init())
+  app.mount('#app')
+} else {
+  const app = createApp(BrowserNote)
+  app.mount('#app')
+}
