87435cecb8620c222c937ecca937050e7d0911ed
mktcode <kontakt@markus-kottlaender.de>
2021-10-04 17:14:39 +0200

    removed old code

--- ui/store.js
+++ ui/store.js
@@ -9,12 +9,6 @@ export default {
     const bareContract = new ethers.Contract(CONTRACT.networks['42'].address, CONTRACT.abi, ethProvider)
     const contract = bareContract.connect(ethSigner)
     const SUBGRAPH_ENDPOINT = 'https://api.thegraph.com/subgraphs/name/web3actions/booster'
-    
-    const barePricefeedContract = new ethers.Contract(
-      '0x9326BFA02ADD2366b30bacB125260Af641031331',
-      [{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}],
-      ethProvider
-    )
 
     return createStore({
       state () {
